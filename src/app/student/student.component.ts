16d3bcea450dbc76bf0141012825707ac26b7301
Adriano Carvalho <beregueder@gmail.com>
2018-02-01 00:34:44 -0200

    Property Binding

---
+++ src/app/student/student.component.ts
@@ -0,0 +1,18 @@
+// tslint:disable-next-line:no-inferrable-types
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jad-student',
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.component.css']
+})
+export class StudentComponent implements OnInit {
+
+  name: string = 'Luke'
+  isJedi: boolean = true
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
